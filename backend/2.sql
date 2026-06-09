ffd98cfc8a4c14dd1b4d09035f0a868742377b66
qianchao555 <xiaoyigedl@gmail.com>
2026-06-09 21:50:07 +0800

    优化

---
+++ backend/2.sql
@@ -0,0 +1,10 @@
+INSERT INTO public.chat_user
+(id, username, display_name, create_time, password_hash, auth_token, "role", department)
+VALUES(3, 'wangwu', '王五', '2026-06-03 09:34:47.927', '$2a$10$/rmboEq2LZPn0gfta4ZHd.h82eKiA8KejYPeGjonixi/cY9qxGa1i', NULL, 'user', '人力资源');
+INSERT INTO public.chat_user
+(id, username, display_name, create_time, password_hash, auth_token, "role", department)
+VALUES(2, 'lisi', '李四', '2026-06-03 09:34:47.927', '$2a$10$6aGIYoKk.6SJ5G2OGIi.0O1JO5CtumlYLc8RFo3KUzDig2thPoKCS', NULL, 'user', '技术');
+INSERT INTO public.chat_user
+(id, username, display_name, create_time, password_hash, auth_token, "role", department)
+VALUES(1, 'zhangsan', '张三', '2026-06-03 09:34:47.927', '$2a$10$Wkk42OmfktmNigZWqfS/0.2c1ePL8whY1GY7YdGo1qJ.yNg6hUrOy', '9ee24d9e093c445fab1c4de8e0d53745', 'admin', '全公司');
+
